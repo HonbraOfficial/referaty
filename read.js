21b6c86f96b9642670dd3360ca430199692b41b6
HonbraOfficial <54026315+HonbraOfficial@users.noreply.github.com>
2020-06-08 14:00:02 +0200

    Update read.js

--- read.js
+++ read.js
@@ -2,6 +2,8 @@ var url_string = window.location.href;
 var url = new URL(url_string);
 var article = url.searchParams.get("!");
 
+const wintitle = document.querySelector("title").innerHTML;
+
 
 if (article) {
     location.href = location.href.split("/")[0] + window.location.pathname + "#" + article;
@@ -19,7 +21,7 @@ function renderArticle() {
     }
     getData(`art/${location.hash.substr(1)}.md`, function(data) {
         document.querySelector("article").innerHTML = marked(data);
-        document.querySelector("title").innerHTML = `${document.querySelector("article h1").innerHTML} - ${document.querySelector("title").innerHTML}`
+        document.querySelector("title").innerHTML = `${document.querySelector("article h1").innerHTML} - ${wintitle}`
     });
 }
 
